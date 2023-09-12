1f9d340c9b673b2cda7e0fc5965989840af834cc
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-12 13:07:20 -0300

    single post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller({})
 export class AppController {
   @Get()
   getUser() {
     return { name: 'Test Person', country: 'Test Country' };
+  }
+
+  @Post()
+  store() {
+    return 'Post request successful';
   }
 }
