1f3448e29c7120d965858cdc781cbc2814a009ad
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-12 12:59:40 -0300

    creation of module and base controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller({})
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUser(){
+    return 'I am a great person';
   }
 }
