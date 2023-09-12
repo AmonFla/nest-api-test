6167cc338a0f2b0035669f7925baef36235f3fc8
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-12 13:12:43 -0300

    accepts params on get request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller({})
 export class AppController {
-  @Get()
-  getUser() {
-    return { name: 'Test Person', country: 'Test Country' };
+  @Get('/:userId')
+  getUser(@Param() params: any) {
+    return { id: params.userId, name: 'Test Person', country: 'Test Country' };
   }
 
   @Post()
