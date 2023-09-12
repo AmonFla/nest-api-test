6ea1877fecd93fbac490c4fd4a3a2bc6a82b29f6
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-12 13:16:56 -0300

    Handling Asynchronous Requests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
+// import { Observable, of } from 'rxjs';
 
 @Controller({})
 export class AppController {
@@ -7,6 +8,16 @@ export class AppController {
   getUser(@Param() params: any) {
     return { id: params.userId, name: 'Test Person', country: 'Test Country' };
   }
+
+  @Get()
+  async findAll(): Promise<any[]> {
+    return [];
+  }
+  /*
+  @Get()
+  findAll(): Observable<any[]> {
+    return of([]);
+  }*/
 
   @Post()
   store(@Req() req: Request) {
