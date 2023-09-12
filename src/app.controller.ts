f49f105723297b63a15ef32485f446b9f9447324
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-12 13:01:18 -0300

    json return

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller({})
 export class AppController {
   @Get()
-  getUser(){
-    return 'I am a great person';
+  getUser() {
+    return { name: 'Test Person', country: 'Test Country' };
   }
 }
