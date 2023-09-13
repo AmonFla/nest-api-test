5e6ade81d0137736228b0dc33ba46f1b72a75439
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-13 15:54:23 -0300

    filtering data through dto

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //solo deja pasar lo que esta en el dto
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
